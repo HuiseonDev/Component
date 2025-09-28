654d20971287f4bc97dba7c486a38f169b61766a
전희선 <designh2sun@gmail.com>
2025-09-28 21:34:46 +0900

    Feat: button 적용 및 상태관리 컴포넌트

---
+++ src/components/Button/ButtonSection.tsx
@@ -0,0 +1,142 @@
+import { css } from "@emotion/react";
+import Button from "./Button";
+import { useState } from "react";
+
+const ButtonSection = () => {
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
+  const ReadyHandler = () => {
+    console.log("기본 버튼 활성화 완료");
+    setIsButtonDisabled(!isButtonDisabled);
+    setIsChecked(!isChecked);
+  };
+
+  const ButtonHandler = () => {
+    console.log("버튼클릭");
+  };
+
+  return (
+    <section>
+      <p>기본 버튼 영역</p>
+
+      <label css={checkboxWrapper} aria-label="기본 버튼 활성화 체크박스">
+        <input css={checkboxStyle} type="checkbox" onChange={ReadyHandler} />
+        <span css={[checkboxBox, isChecked && checkedBox]} />
+        <span>기본 버튼 활성화</span>
+      </label>
+
+      <div
+        css={css`
+          display: flex;
+          gap: 1rem;
+          align-items: center;
+          margin-bottom: 1rem;
+        `}
+      >
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="small"
+          width="fit"
+          BasicButtontext={"small-fit"}
+        />
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="small"
+          width="max"
+          BasicButtontext={"small-max"}
+        />
+      </div>
+      <div
+        css={css`
+          display: flex;
+          gap: 1rem;
+          align-items: center;
+          margin-bottom: 1rem;
+        `}
+      >
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="medium"
+          width="fit"
+          BasicButtontext={"medium-fit"}
+        />
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="medium"
+          width="max"
+          BasicButtontext={"medium-max"}
+        />
+      </div>
+      <div
+        css={css`
+          display: flex;
+          gap: 1rem;
+          align-items: center;
+          margin-bottom: 1rem;
+        `}
+      >
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="large"
+          width="fit"
+          BasicButtontext={"large-fit"}
+        />
+        <Button
+          onClick={ButtonHandler}
+          disabled={isButtonDisabled}
+          size="large"
+          width="max"
+          BasicButtontext={"large-max"}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ButtonSection;
+
+const checkboxWrapper = css`
+  display: flex;
+  position: relative;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const checkboxStyle = css`
+  position: absolute;
+  opacity: 0;
+`;
+
+const checkboxBox = css`
+  width: 2rem;
+  height: 2rem;
+  background-color: #ffffff;
+  border: 1px solid #ccc;
+  position: relative;
+  border-radius: 0.4rem;
+`;
+
+const checkedBox = css`
+  width: 2rem;
+  height: 2rem;
+  border-color: #3182f6;
+  background-color: #3182f6;
+  border-radius: 0.4rem;
+
+  &::after {
+    content: "✔︎";
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1rem;
+  }
+`;
