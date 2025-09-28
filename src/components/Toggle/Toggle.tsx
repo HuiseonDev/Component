317d8a4b77a66ac3d3b8125f4c2657bd1b77cb88
전희선 <designh2sun@gmail.com>
2025-09-28 21:33:01 +0900

    Feat: 토글컴포넌트

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,39 @@
+import { css } from "@emotion/react";
+import { useState } from "react";
+
+const Toggle = () => {
+  const [isOn, setIsOn] = useState<boolean>(false);
+
+  const handleToggle = () => {
+    setIsOn(!isOn);
+  };
+
+  return (
+    <button css={toggleButton(isOn)} onClick={handleToggle}>
+      <span css={toggleCircle(isOn)} />
+    </button>
+  );
+};
+
+export default Toggle;
+
+const toggleButton = (isOn: boolean) => css`
+  width: 5rem;
+  height: 2rem;
+  background-color: ${isOn ? `#ffff` : `#eeee`};
+  border: 1px solid #aaaa;
+  cursor: pointer;
+  border-radius: 1rem;
+  transition: background-color 0.2s ease;
+  display: flex;
+  padding: 0.4rem;
+`;
+
+const toggleCircle = (isOn: boolean) => css`
+  width: 1.2rem;
+  height: 1.2rem;
+  background-color: ${isOn ? `black` : `#9999`};
+  border-radius: 50%;
+  transform: ${isOn ? "translateX(3rem)" : "translateX(0)"};
+  transition: transform 0.2s ease;
+`;
