d1be372f5fc55fbc6c78c6337c7fbc7302af5873
전희선 <designh2sun@gmail.com>
2025-09-28 21:35:59 +0900

    Feat: BottomCTA 적용 및 상태관리 컴포넌트

---
+++ src/components/BottomCTA/BottomCTASection.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import BottomCTA from "./BottomCTA";
+
+const BottomCTASection = () => {
+  const [isCTADisabled, setIsCTADisabled] = useState<boolean>(true);
+
+  const handleReady = () => {
+    console.log("바텀CTA 버튼 활성화 완료");
+    setIsCTADisabled(!isCTADisabled);
+  };
+
+  //** 이동하는 로직 추가 */
+  const handleBottomButton = () => {
+    console.log("이동");
+  };
+
+  return (
+    <section>
+      <p>bottom CTA 영역</p>
+      <label aria-label="바텀 CTA 버튼 활성화 체크박스">
+        <input type="checkbox" onChange={handleReady} />
+        <span>바텀CTA 버튼 활성화</span>
+      </label>
+      <BottomCTA
+        bottomCTADisabled={isCTADisabled}
+        bottomCTAText="이동하기"
+        bottomCTAHandler={handleBottomButton}
+      />
+    </section>
+  );
+};
+
+export default BottomCTASection;
