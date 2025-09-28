1370daab53cb7746d8ff73a21df88149d1a29e3c
전희선 <designh2sun@gmail.com>
2025-09-28 21:33:31 +0900

    Feat: Filter 컴포넌트

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,88 @@
+import { css } from "@emotion/react";
+import { useState } from "react";
+
+interface FilterProps {
+  categories: string[];
+  selectedFilter: string;
+  setSelectedFilter: (value: string) => void;
+}
+
+const Filter = ({
+  categories,
+  selectedFilter,
+  setSelectedFilter,
+}: FilterProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // 필터링 적용 핸들러
+  const FilterHandler = (c: string) => {
+    setSelectedFilter(c);
+  };
+
+  // 초기화 핸들러
+  const resetHandler = () => {
+    setSelectedFilter("");
+  };
+
+  const dropDownHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <p>필터링 영역</p>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <div css={dropDownWrapper}>
+          <button onClick={dropDownHandler}>
+            {selectedFilter ? selectedFilter : "선택하기"}
+          </button>
+          {isOpen && (
+            <ul css={dropdownbox}>
+              {categories.map((c: string) => (
+                <li css={dropItem} onClick={() => FilterHandler(c)}>
+                  {c}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <button
+          css={css`
+            box-sizing: border-box;
+            height: fit-content;
+          `}
+          onClick={resetHandler}
+        >
+          초기화
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Filter;
+
+const dropDownWrapper = css`
+  background-color: #ccc;
+  display: flex;
+  flex-direction: column;
+  width: 5rem;
+`;
+
+const dropdownbox = css`
+  display: flex;
+  flex-direction: column;
+  padding: 0 1rem;
+  gap: 1rem;
+`;
+
+const dropItem = css`
+  list-style: none;
+  cursor: pointer;
+  align-items: center;
+  text-align: center;
+`;
